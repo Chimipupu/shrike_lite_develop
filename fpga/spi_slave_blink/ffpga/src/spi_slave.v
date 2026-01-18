2e248cc38b2791f573c555014105b885249306c8
Chimipupu <chiminyanyan@gmail.com>
2026-01-19 01:01:19 +0900

    SPIスレーブの送信回路を修正（動作確認はまだ未実施）

--- fpga/spi_slave_blink/ffpga/src/spi_slave.v
+++ fpga/spi_slave_blink/ffpga/src/spi_slave.v
@@ -1,72 +1,85 @@
 module spi_slave(
+    // クロック・リセット入力ピン
     input i_clk,              // クロック = 50MHz
     input i_rst_n,            // リセット入力(RSTn = アクティブLOW)
 
+    // SPIスレーブピン
     input i_spi_s_sck,        // SPI SCKピン
     input i_spi_s_cs_n,       // SPI CSnピン(アクティブLOW)
-
     input i_spi_s_mosi,       // SPI MOSIピン
-    output reg o_spi_s_rx_done,
-    output reg [7:0] o_spi_s_rx_data,
-
     output o_spi_s_miso_oe,   // SPI MISOピンのOEピン
     output reg o_spi_s_miso,  // SPI MISOピン
-    output reg o_spi_s_tx_done,
-    output reg [7:0] o_spi_s_tx_data
+
+    // LEDピン
+    output reg o_led,
+    output o_led_en
     );
 
     // MISOピンは常に出力有効
     assign o_spi_s_miso_oe = 1'b1;
+    // LEDを有効化
+    assign o_led_en = 1'b1;
 
     // ビットカウンタ(RX, TX)
     reg [2:0] r_rx_bit_cnt;
     reg [2:0] r_tx_bit_cnt;
 
+    // 送受信FIFO
+    reg r_rx_done;
+    reg [7:0] r_rx_data;
+    reg r_tx_done;
+    reg r_tx_data_set_done;
+    reg [7:0] r_tx_data;
+
     // --- シンクロナイザ（外部信号を内部クロックに同期） ---
     reg [1:0] r_sck_sync;
     reg [1:0] r_cs_sync;
     reg [1:0] r_mosi_sync;
+    reg [1:0] r_miso_sync;
 
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
-            r_sck_sync  <= 2'b11; // Idle High想定なら0b11, Lowなら0b00
-            r_cs_sync   <= 2'b11; // Active Lowなので初期値High
+            r_sck_sync  <= 2'b11;
+            r_cs_sync   <= 2'b11;
             r_mosi_sync <= 2'b00;
+            r_miso_sync <= 2'b00;
         end else begin
             r_sck_sync  <= {r_sck_sync[0],  i_spi_s_sck};
             r_cs_sync   <= {r_cs_sync[0],   i_spi_s_cs_n};
             r_mosi_sync <= {r_mosi_sync[0], i_spi_s_mosi};
+            r_miso_sync <= {r_miso_sync[0], o_spi_s_miso};
         end
     end
 
     wire w_sck_sync  = r_sck_sync[1];
     wire w_cs_sync   = r_cs_sync[1];
     wire w_mosi_sync = r_mosi_sync[1];
+    wire w_miso_sync = r_miso_sync[1];
 
+    // SCKエッジ検出(立ち上がり/立ち下がり)
     wire w_sck_posedge = (r_sck_sync[1:0] == 2'b01);
+    wire w_sck_negedge = (r_sck_sync[1:0] == 2'b10);
 
     // SPI受信処理
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
-            r_rx_bit_cnt       <= 3'd0;
-            o_spi_s_rx_data <= 8'd0;
-            o_spi_s_miso    <= 1'b0;
-            o_spi_s_rx_done <= 1'b0;
+            r_rx_data <= 8'd0;
+            r_rx_bit_cnt <= 3'd0;
+            r_rx_done <= 1'b0;
         end else begin
-            o_spi_s_rx_done <= 1'b0;
+            r_rx_done <= 1'b0;
 
             // CSn = Low のときSPI通信有効
             if (w_cs_sync == 1'b0) begin
                 // SCKの立ち上がりエッジのみで動作
                 if (w_sck_posedge) begin
                     // シフトイン (MSBファースト)
-                    o_spi_s_rx_data <= {o_spi_s_rx_data[6:0], w_mosi_sync};
+                    r_rx_data <= {r_rx_data[6:0], w_mosi_sync};
                     // ビットカウントアップ
                     r_rx_bit_cnt <= r_rx_bit_cnt + 1'b1;
                     // 8ビット受信完了判定
                     if (r_rx_bit_cnt == 3'd7) begin
-                        o_spi_s_rx_done <= 1'b1; // 1クロックだけHighになる
-                        // TODO: データのコピーやリセット
+                        r_rx_done <= 1'b1; // 1クロックだけHighになる
                     end
                 end
             end else begin
@@ -80,33 +93,51 @@ module spi_slave(
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
             r_tx_bit_cnt <= 3'd0;
-            o_spi_s_tx_data <= 8'd0;
-            o_spi_s_tx_done <= 1'b0;
-            o_spi_s_miso    <= 1'b0;
+            r_tx_done <= 1'b0;
+            o_spi_s_miso <= 1'b0;
         end else begin
-            o_spi_s_tx_done <= 1'b0;
-
+            r_tx_done <= 1'b0;
             // CSn = Low のときSPI通信有効
             if (w_cs_sync == 1'b0) begin
-                // SCKの立ち上がりエッジのみで動作
-                if (w_sck_posedge) begin
-                    // MISOピンにデータをセット (MSBファースト)
-                    o_spi_s_miso <= o_spi_s_tx_data[7];
-
-                    // シフトアウト
-                    o_spi_s_tx_data <= {o_spi_s_tx_data[6:0], 1'b0};
-
+                // SCKの立ち下がりエッジで動作
+                if (w_sck_negedge) begin
+                    // MISOピンにデータを1bitセット (MSBファースト)
+                    o_spi_s_miso <= r_tx_data[7 - r_tx_bit_cnt];
                     // ビットカウントアップ
                     r_tx_bit_cnt <= r_tx_bit_cnt + 1'b1;
-
                     // 8ビット送信完了判定
                     if (r_tx_bit_cnt == 3'd7) begin
-                        o_spi_s_tx_done <= 1'b1; // 1クロックだけHighになる
+                        r_tx_done <= 1'b1;
+                        r_tx_data_set_done <= 1'b0;
                     end
                 end
             end else begin
                 // CSn = Highになったらカウンタをリセット
                 r_tx_bit_cnt <= 3'd0;
+            end
+        end
+    end
+
+    // 送信データ準備
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if(!i_rst_n) begin
+            o_led <= 1'b0;
+            r_tx_data <= 8'd0;
+        // CSnがHighになったら送信データをクリア
+        end else if (w_cs_sync == 1'b1) begin
+            r_tx_data <= 8'd0;
+        end else if(r_rx_done) begin
+            // SCKの立ち下がりエッジで動作
+            if (w_sck_negedge) begin
+                // 受信データ = 0xAAならLEDをON
+                if(r_rx_data == 8'hAA) begin
+                    o_led <= 1'b1;
+                    r_tx_data <= 8'h55;
+                // 受信データ = 0x55ならLEDをOFF
+                end else if(r_rx_data == 8'h55) begin
+                    o_led <= 1'b0;
+                    r_tx_data <= 8'hAA;
+                end
             end
         end
     end
--- fpga/spi_slave_blink/ffpga/src/top.v
+++ fpga/spi_slave_blink/ffpga/src/top.v
@@ -6,8 +6,8 @@
     (* iopad_external_pin *) output o_clk_en,
     // SPIスレーブピン
     (* iopad_external_pin *) input i_spi_s_sck,
-    (* iopad_external_pin *) input i_spi_s_mosi,
     (* iopad_external_pin *) input i_spi_s_cs_n,
+    (* iopad_external_pin *) input i_spi_s_mosi,
     (* iopad_external_pin *) output o_spi_s_miso,
     (* iopad_external_pin *) output o_spi_s_miso_oe,
     // LEDピン
@@ -15,29 +15,7 @@
     (* iopad_external_pin *) output o_led_en
     );
 
-    wire w_spi_s_rx_done;
-    wire [7:0] w_spi_s_rx_data;
-
-    wire w_spi_s_tx_done;
-    wire [7:0] w_spi_s_tx_data;
-
-    assign o_led_en = 1'b1; // LEDを有効化
     assign o_clk_en = 1'b1; // クロックを有効化
-
-    // SPIの受信したデータ解析
-    always @(posedge i_clk or negedge i_rst_n) begin
-        if(!i_rst_n) begin
-            o_led <= 1'b0;
-        end else if(w_spi_s_rx_done) begin
-            // 受信データ = 0xAAならLEDをON
-            if(w_spi_s_rx_data == 8'hAA) begin
-                o_led <= 1'b1;
-            // 受信データ = 0x55ならLEDをOFF
-            end else if(w_spi_s_rx_data == 8'h55) begin
-                o_led <= 1'b0;
-            end
-        end
-    end
 
     // SPIスレーブ
     spi_slave u_spi_slave (
@@ -48,10 +26,8 @@
         .i_spi_s_mosi(i_spi_s_mosi),
         .o_spi_s_miso(o_spi_s_miso),
         .o_spi_s_miso_oe(o_spi_s_miso_oe),
-        .o_spi_s_rx_done(w_spi_s_rx_done),
-        .o_spi_s_rx_data(w_spi_s_rx_data),
-        .o_spi_s_tx_done(w_spi_s_tx_done),
-        .o_spi_s_tx_data(w_spi_s_tx_data)
+        .o_led(o_led),
+        .o_led_en(o_led_en)
     );
 
 endmodule
